3c80c127abe65e51df9696dfc4c628eba4f09cdf
Lisa Muraya <lizamuraya@gmail.com>
2023-06-26 14:56:09 +0300

    changes

--- toDoList/toDoList/AppDelegate.swift
+++ toDoList/toDoList/AppDelegate.swift
@@ -1,9 +1,4 @@
-//
-//  AppDelegate.swift
-//  toDoList
-//
-//  Created by MacBookAir on 24.06.2023.
-//
+
 
 import UIKit
 
@@ -13,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
         return true
     }
 
